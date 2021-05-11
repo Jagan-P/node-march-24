3028d01ef6b42c5b86240a07b1575ee3d8bbe0ec
Jagan.P <jagan.p@plantix.net>
2021-05-11 09:56:20 +0530

    parsing url encoded body

--- express-server.js
+++ express-server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.raw({}));
+// app.use(bodyParser.json({}));
+// app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -11,6 +17,7 @@ app.get('/users',(req,res)=>{
 });
 
 app.post('/users',(req,res)=>{
+    console.log(req.body); 
     res.send("post users");
 });
 
